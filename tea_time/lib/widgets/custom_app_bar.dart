8f57bfdfc847b80fc39f1d73acf862ab9d0a7e5b
stephaneleonard <stefleonard97@gmail.com>
2021-11-13 17:20:45 +0000

    Update custom_app_bar.dart

--- tea_time/lib/widgets/custom_app_bar.dart
+++ tea_time/lib/widgets/custom_app_bar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   const CustomAppBar({required this.title, Key? key}) : super(key: key);
@@ -12,27 +11,9 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.only(top: 10),
-            child: SvgPicture.asset(
-              'assets/svg/logo.svg',
-              height: 90,
-              color: Colors.green,
-            ),
-          ),
-          Text(
-            title,
-            style: const TextStyle(
-                color: Colors.black, fontSize: 48, fontWeight: FontWeight.bold),
-          ),
-        ],
+      title: Text(
+        title,
       ),
-      backgroundColor: Colors.white,
-      iconTheme: const IconThemeData(color: Colors.black),
       centerTitle: true,
     );
   }
