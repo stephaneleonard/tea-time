3ba5330b1a61f73a3608b5fc3bb87f2611207780
stephaneleonard <stefleonard97@gmail.com>
2021-11-13 17:18:11 +0000

    Update theming.dart

--- tea_time/lib/core/theming/theming.dart
+++ tea_time/lib/core/theming/theming.dart
@@ -4,4 +4,25 @@ ThemeData lightTheme = ThemeData(
   brightness: Brightness.light,
   primarySwatch: Colors.green,
   visualDensity: VisualDensity.adaptivePlatformDensity,
+  appBarTheme: const AppBarTheme(
+    backgroundColor: Colors.white,
+    titleTextStyle: TextStyle(
+        color: Colors.black, fontSize: 32, fontWeight: FontWeight.bold),
+  ),
+  textTheme: const TextTheme(
+    headline3: TextStyle(color: Colors.black),
+  ),
+);
+
+ThemeData darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  primarySwatch: Colors.green,
+  visualDensity: VisualDensity.adaptivePlatformDensity,
+  appBarTheme: const AppBarTheme(
+    titleTextStyle: TextStyle(
+        color: Colors.white, fontSize: 32, fontWeight: FontWeight.bold),
+  ),
+  textTheme: const TextTheme(
+    headline3: TextStyle(color: Colors.white),
+  ),
 );
